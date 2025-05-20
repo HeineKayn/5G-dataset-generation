3ba5578fa8ce5841831b50f4bc29e8ba998da6a0
nxvertime <nxvertime@gmail.com>
2025-05-20 10:47:42 +0200

    Refactor import paths in PFCP attack scripts and utilities for consistency

--- src/attacks/upf_pfcp/pfcpDosAttack.py
+++ src/attacks/upf_pfcp/pfcpDosAttack.py
@@ -1,11 +1,11 @@
 # from pfcpToolkit import PFCPToolkit
-from src.attacks.upf_pfcp.pfcpToolkit import PFCPToolkit
+from src.pfcpToolkit.pfcpToolkit import PFCPToolkit
 import random, time, threading, ipaddress
 from scapy.all import send, sr1
 
 from scapy.contrib.pfcp import *
-from src.attacks.upf_pfcp.utils.handleParams import HandleParams
-from src.attacks.upf_pfcp.utils.logger import Log
+from src.pfcpToolkit.utils.logger import Log
+from src.pfcpToolkit.utils.handleParams import HandleParams
 
 
 # ---------------------------------------------------------------------------- #
